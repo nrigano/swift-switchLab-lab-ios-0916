4e8b33096cdb454834a51c9b878e103510635253
Nick Rigano <nrigano@Executor.local>
2016-09-21 08:58:00 -0400

    completed

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -11,7 +11,7 @@
 //: First let's try some of last lab's questions but using a 'switch' instead of 'if' or 'if/else'. Can you figure out the types of `x`, `y`, `a`, and `b` without option clicking the constants?
 
 // Given....
-let x = 5.0
+let x = 5.0 //Double; the others are Ints
 let y = 12
 let a = 321
 let b = 32
@@ -24,18 +24,23 @@ let b = 32
 */
 
 
-
-
-
-
+switch a % b == 0 {
+    case true:
+        print("true")
+    default:
+        print("false")
+}
 
 
 /*: Question 2
 ### **(2)** Print 'true' if y divided by x is greater than three, otherwise print 'false'.
 */
-
-
-
+switch Double(y) / x > 3 {
+    case true:
+        print("true")
+    default:
+        print("false")
+}
 
 
 
@@ -43,10 +48,12 @@ let b = 32
 /*: Question 3
 ### **(3)** Print 'true' if y is greater than x and a divided by b is greater than 9. If there's any need to convert to either an `Int` or a `Double` to satisfy the requirements of a binary operator (you can choose whichever one you want to convert to to satisfy the requirements).
 */
-
-
-
-
+switch Double(y) > x && a / b > 9 {
+case true:
+    print("true")
+default:
+    print("false")
+}
 
 
 
@@ -54,10 +61,18 @@ let b = 32
 /*: Question 4
 ### **(4)** Write a function named `isGreater` that takes two `Int` arguments and returns a `String`. This function should look to see if `a` is greater than `b`. If it is, it returns back the `String` 'yep', else it returns back the `String` 'nope`.
 */
+func isGreater(firstNumber: Int, secondNumber: Int) -> String {
+    switch a > b {
+    case true:
+        var theAnswerIs = "yep"
+        return theAnswerIs
+    case false:
+        var theAnswerIs = "nope"
+        return theAnswerIs
+    }
+}
 
-
-
-
+isGreater(firstNumber: a, secondNumber: b)
 
 
 
@@ -70,11 +85,37 @@ let b = 32
  isForce(with:"Mary") // false
  ```
 */
+func isForce(jedi: String) -> Bool {
+    switch jedi {
+        case "Luke":
+            let midichlorians = true
+            return midichlorians
+        
+        case "Leia":
+            let midichlorians = true
+            return midichlorians
 
+        case "Anakin":
+            let midichlorians = true
+            return midichlorians
 
-
-
-
+        case "Obi Wan":
+            let midichlorians = true
+            return midichlorians
+                
+        case "Yoda":
+            let midichlorians = true
+            return midichlorians
+                
+        case "Vader":
+            let midichlorians = true
+            return midichlorians
+                
+        default:
+            let midichlorians = false
+            return midichlorians
+    }
+}
 
 
 
@@ -88,8 +129,18 @@ let b = 32
 */
 
 
-
-
+    func isInRange(measurement: Int) {
+        switch measurement {
+        case 0...3:
+            print("small")
+        case 4...6:
+            print("medium")
+        case 7...10:
+            print("big")
+        default:
+            print("not sure")
+        }
+}
 
 
 
